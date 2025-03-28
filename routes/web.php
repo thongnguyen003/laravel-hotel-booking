00ce0369f21e84b47e6578135d8d8c25a091a0d9
HuuHau235 <hhai98940@gmail.com>
2025-03-28 15:47:12 +0700

    feature/26-Build interface of the marked room management page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+
+Route::get('/profile', function () {
+    return view('pages.profile');
+});
+
+Route::get('/booked', function () {
+    return view('pages.booked');
+});
+
+Route::get('/mark', function () {
+    return view('pages.mark');
 });
