b43c9e872047920408439a3c693aa3c0030ebe2b
unknown <admin@pnvl151.pn.com.vn>
2025-03-30 08:54:50 +0700

    feature/18/Code forgotPassword Page/fix route

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,23 @@
 <?php
 
+use App\Http\Controllers\ForgotPasswordController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Fake_user;
 
-
 // Trang chủ trang web
 Route::get('/',[k::class,'index']);
+// quên mật khẩu
+Route::get('/forgotPw',[ForgotPasswordController::class,'forgotPassword'])->name('fpw');
+// gửi mail khi quên mật khẩu
+Route::get('/confirm-forgotPw',[ForgotPasswordController::class,'confirmPassword'])->name('confirm-fpw');
 // Trang đăng ký người dùng
 Route::get ('/register',[k::class,'register'])->name('register');
-
 // Trang đăng nhập người dùng
 Route::get('/login',[k::class,'login'])->name('log-in');
 
 // Nhóm các route có tiền tố là là profile và cần đăng nhập mới truy cập được
 Route::prefix('profile')->group(function(){
+
     // Trang hồ sơ các nhân
     Route::get('/',[Fake_user::class,'profile'])->name('profile');
 
@@ -29,4 +33,3 @@ Route::prefix('profile')->group(function(){
     // Trang danh sách phòng đã đặt
     Route::get('/booked_room',[Fake_user::class,'booked_room'])->name('history');
 });
-
