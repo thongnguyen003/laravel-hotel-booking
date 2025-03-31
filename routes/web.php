55c2578f9a208ab99bf7ac398c00f0a481d44516
unknown <admin@pnvl151.pn.com.vn>
2025-03-31 15:53:23 +0700

    feature 18/fix routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Fake_user;
 use App\Http\Controllers\ForgetPasswordManager;
+// use App\Http\Controllers\Auth\ForgotPasswordControllers;
 
 // thử lại 
 Route::get('/forget-password', [ForgetPasswordManager::class, 'ForgetPassword'])
@@ -14,53 +14,21 @@ Route::get('/reset-password/{token}', [ForgetPasswordManager::class,'resetPasswo
 ->name("reset-password");
 Route::post('/reset-password', [ForgetPasswordManager::class,'resetPasswordPost'])
 ->name("reset-passwordPost");
-// // Trang chủ trang web
-// Route::get('/',[k::class,'index']);
-// quên mật khẩu
-// Route::get('/forgotPw',[ForgotPasswordControllers::class,'forgotPassword'])->name('fpw');
-// // gửi mail khi quên mật khẩu
-// Route::post('/forgotPw',[ForgotPasswordControllers::class,'forgotPasswordPost'])->name('fpwP');
-// // Trang đăng ký người dùng
-// Route::get ('/register',[k::class,'register'])->name('register');
-// // Trang đăng nhập người dùng
-// Route::get('/login',[k::class,'login'])->name('log-in');
-
-// Trang chủ trang web
-Route::get('/', [Fake_user::class, 'index']);
-
-// Trang đăng ký người dùng
-Route::get('/register', [Fake_user::class, 'register'])->name('register');
-
-// Trang đăng nhập người dùng
-Route::get('/login', [Fake_user::class, 'login'])->name('log-in');
-
 // Nhóm các route có tiền tố là là profile và cần đăng nhập mới truy cập được
 Route::prefix('profile')->group(function(){
 
-Route::prefix('profile')->group(function () {
-  
     // Trang hồ sơ các nhân
-    Route::get('/', [Fake_user::class, 'profile'])->name('profile');
+    Route::get('/',[Fake_user::class,'profile'])->name('profile');
 
     // Trang cập nhập hồ sơ người dùng
-    Route::get('/edit', [Fake_user::class, 'edit_profile'])->name('edit_profile');
+    Route::get('/edit',[Fake_user::class,'edit_profile'])->name('edit_profile');
 
     // Cập nhật hồ sơ người dùng
-    Route::post('/update', [Fake_user::class, 'update_profile'])->name('update_profile');
-    Route::post('/update_avatar', [Fake_user::class, 'update_avatar_profile'])->name('update_avatar_profile');
+    Route::post('/',[Fake_user::class,'update_profile'])->name('update_profile');
 
     // Trang danh sách phòng đánh dấu
-    Route::get('/marked_room', [Fake_user::class, 'marked_room'])->name('mark');
-
+    Route::get('/marked_room',[Fake_user::class,'marked_room'])->name('mark');
+    
     // Trang danh sách phòng đã đặt
-    Route::get('/booked_room', [Fake_user::class, 'booked_room'])->name('history');
+    Route::get('/booked_room',[Fake_user::class,'booked_room'])->name('history');
 });
-
-
-Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-
