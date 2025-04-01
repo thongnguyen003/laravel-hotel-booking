8dfa43b24729dc2fe8fe90d4971762dae0339d63
thongnguyen003 <nguyenthong0855@gmail.com>
2025-04-01 11:01:11 +0700

    Feature/18 (#11)
    * feature/18/Code forgotPassword Page
    
    * feature/18/Code forgotPassword Page/fix view
    
    * feature/18/Code forgotPassword Page/fix route
    
    * feature/18 fix controller
    
    * feature 18/fix routes
    
    * feature 18/ done
    
    ---------
    
    Co-authored-by: unknown <admin@pnvl151.pn.com.vn>

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Fake_user;
+use App\Http\Controllers\ForgetPasswordManager;
+use App\Http\Controllers\AuthController;
+
+// thử lại 
+Route::get('/forget-password', [ForgetPasswordManager::class, 'ForgetPassword'])
+->name('forgetPassword');
+Route::post('/forget-password', [ForgetPasswordManager::class, 'ForgetPasswordPost'])
+->name('forgetPasswordPost');
+Route::get('/reset-password/{token}', [ForgetPasswordManager::class,'resetPassword'])
+->name("reset-password");
+Route::post('/reset-password', [ForgetPasswordManager::class,'resetPasswordPost'])
+->name("reset-passwordPost");
 
 // Trang chủ trang web
-Route::get('/', [Fake_user::class, 'index']);
-
+// Route::get('/', [Fake_user::class, 'index']);
 // Trang đăng ký người dùng
 Route::get('/register', [Fake_user::class, 'register'])->name('register');
 
@@ -15,7 +25,6 @@ Route::get('/login', [Fake_user::class, 'login'])->name('log-in');
 
 // Nhóm các route có tiền tố là là profile và cần đăng nhập mới truy cập được
 Route::prefix('profile')->group(function () {
-  
     // Trang hồ sơ các nhân
     Route::get('/', [Fake_user::class, 'profile'])->name('profile');
 
@@ -40,4 +49,3 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-
