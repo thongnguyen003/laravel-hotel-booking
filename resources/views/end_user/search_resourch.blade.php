783ad1a9d821a4c26e3c696282080be44a24db5f
thongnguyen003 <nguyenthong0855@gmail.com>
2025-04-04 23:57:06 +0700

    feature/29-develop filtering and searching features

--- resources/views/end_user/search_resourch.blade.php
+++ resources/views/end_user/search_resourch.blade.php
@@ -5,6 +5,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
     <script>
         function toggleSortOptions(action) {
+            event.preventDefault();
             if(action == 'type'){
                 const options = document.getElementById('typeOptions');
                 options.classList.toggle('hidden');
@@ -14,13 +15,15 @@
             }else if (action == 'capacity'){
                 const options = document.getElementById('capacityOptions');
                 options.classList.toggle('hidden');
+            }else if (action == 'price'){
+                const options = document.getElementById('priceOptions');
+                options.classList.toggle('hidden');
             }
         }
     </script>
 </head>
 <body class="bg-gray-100">
     <!-- Header -->
-     <p>{{$rooms}}</p>
     <header class="bg-teal-500 p-4 flex justify-between items-center">
         <div class="flex items-center">
             <img alt="Hotel Logo" class="h-12 w-12" height="50" src="https://storage.googleapis.com/a1aa/image/nCjm3F3FksKW0bcC9qzx2OnZtxDp3h30Cs-AcByAfyQ.jpg" width="50"/>
@@ -40,10 +43,17 @@
     <!-- Main Content -->
     <main class="p-4">
         <div class="flex justify-between items-center mb-4">
-            <div class="flex space-x-4 w-3/4 bg-white p-4 rounded shadow">
-                <!-- <select class="p-2 border rounded flex-1">
-                    <option>Type Room</option>
-                </select> -->
+            <form class="flex space-x-4 w-3/4 bg-white p-4 rounded shadow" action="/search" method="POST">
+                @csrf
+                <!-- các input -->
+                <input type="hidden" id="selectedRoomTypes" name="type_id" value="{{$type_id}}">
+                <input type="hidden" id="selectedRoomCapacities" name="capacity_id" value="{{$capacity_id}}">
+                <input type="hidden" id="selectedSort" name="method_sort" value="">
+                <input type="hidden" id="minInput" name="minimum">
+                <input type="hidden" id="maxInput" name="maximum">
+                <input type="hidden"  name="inception_day" value="{{$start_day}}">
+                <input type="hidden"  name="expiration_day" value="{{$end_day}}">
+                
                 <div class="relative flex-1">
                     <button class="p-2 border rounded flex items-center justify-center w-full" onclick="toggleSortOptions('type')">
                         <i class="fas fa-bars mr-2"></i>
@@ -51,25 +61,16 @@
                     </button>
                     <div id="typeOptions" class="absolute left-0 right-0 bg-white border rounded mt-2 hidden z-10">
                         <ul>
-                            <li class="p-2 border-b">Option 1</li>
-                            <li class="p-2 border-b">Option 2</li>
-                            <li class="p-2">Option 3</li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- <select class="p-2 border rounded flex-1">
-                    <option>Capacity</option>
-                </select> -->
-                <div class="relative flex-1">
-                    <button class="p-2 border rounded flex items-center justify-center w-full" onclick="toggleSortOptions('capacity')">
-                        <i class="fas fa-bars mr-2"></i>
-                        <span>Room Capacity</span>
-                    </button>
-                    <div id="capacityOptions" class="absolute left-0 right-0 bg-white border rounded mt-2 hidden z-10">
-                        <ul>
-                            <li class="p-2 border-b">Option 1</li>
-                            <li class="p-2 border-b">Option 2</li>
-                            <li class="p-2">Option 3</li>
+                            @php
+                                $type_id = explode(',',$type_id) ;
+                                !empty($capacity_id) ? $capacity_id = explode(',',$capacity_id) : null ;
+                            @endphp
+                            @foreach($types as $type)
+                            <li class="p-2 border-b flex items-center">
+                                <input type="checkbox"  name="roomType" value="{{ $type->id}}" class="mr-2 type_room"{{ is_array($type_id) ? (in_array($type->id,$type_id)? 'checked' : '') : ''}}>
+                                <label for="option1">{{ $type->type_name}}</label>
+                            </li>
+                            @endforeach
                         </ul>
                     </div>
                 </div>
@@ -80,15 +81,34 @@
                     </button>
                     <div id="capacityOptions" class="absolute left-0 right-0 bg-white border rounded mt-2 hidden z-10">
                         <ul>
-                            <li class="p-2 border-b">Option 1</li>
-                            <li class="p-2 border-b">Option 2</li>
-                            <li class="p-2">Option 3</li>
+                            @foreach($capacities as $capacity)
+                                <li class="p-2 border-b flex items-center">
+                                    <input type="checkbox"  name="roomCapacity" value="{{ $capacity->id}}" class="mr-2 capacity_room" {{ is_array($capacity_id) ? (in_array($capacity->id,$capacity_id)? 'checked' : '') : ''}}>
+                                    <label for="option1">{{ $capacity->capacity}}</label>
+                                </li>
+                            @endforeach
                         </ul>
                     </div>
                 </div>
-                <!-- <select class="p-2 border rounded flex-1">
-                    <option>Price</option>
-                </select> -->
+                <div class="relative flex-1">
+                    <button class="p-2 border rounded flex items-center justify-center w-full" onclick="toggleSortOptions('price')">
+                        <i class="fas fa-bars mr-2"></i>
+                        <span>Room price</span>
+                    </button>
+                    <div id="priceOptions" class="absolute left-0 right-0 bg-white border rounded mt-2 hidden z-10 p-3">
+                        <div class="container">
+                            <h4>Chọn khoảng giá:</h4>
+                            <div class="mb-3">
+                                <p>Min: <span id="minValue"></span></p>
+                                <input type="range" id="minRange" min="0" max="{{$maxPrice}}" value="0" class="form-range">
+                            </div>
+                            <div>
+                                <p>Max: <span id="maxValue"></span></p>
+                                <input type="range" id="maxRange" min="0" max="{{$maxPrice}}" value="{{$maxPrice}}" class="form-range">
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="relative flex-1">
                     <button class="p-2 border rounded flex items-center justify-center w-full" onclick="toggleSortOptions('sort')">
                         <i class="fas fa-bars mr-2"></i>
@@ -96,13 +116,23 @@
                     </button>
                     <div id="sortOptions" class="absolute left-0 right-0 bg-white border rounded mt-2 hidden z-10">
                         <ul>
-                            <li class="p-2 border-b">Option 1</li>
-                            <li class="p-2 border-b">Option 2</li>
-                            <li class="p-2">Option 3</li>
+                            <li class="p-2 border-b flex items-center">
+                                <input type="radio"  name="sort" value="asc" class="mr-2 sort_room">
+                                <label for="option1">Ascending </label>
+                            </li>
+                            <li class="p-2 border-b flex items-center">
+                                <input type="radio"  name="sort" value="desc" class="mr-2 sort_room">
+                                <label for="option1">Desscending </label>
+                            </li>
                         </ul>
                     </div>
                 </div>
-            </div>
+                <div class="relative flex-1">
+                    <button type="submit" class="p-2 border rounded flex items-center justify-center w-full bg-blue-500 text-white hover:bg-blue-600 transition">
+                        <span>Call</span>
+                    </button>
+                </div>
+            </form>
             <div class="w-1/4 bg-white p-4 rounded shadow ml-4">
                 <button class="p-2 border rounded w-full">Information booking</button>
             </div>
@@ -143,7 +173,7 @@
             <div class="w-1/4 ml-4">
                 <div class="bg-white p-4 rounded shadow">
                     <h3 class="text-lg font-bold">Hotel name</h3>
-                    <p class="text-gray-500">22/03/2025 - 25/03/2025 (3 nights)</p>
+                    <p class="text-gray-500">{{$start_day}} - {{$end_day}} </p>
                     <div class="mt-4">
                         <div class="flex justify-between items-center">
                             <span>Superior Twin Room 230</span>
@@ -221,5 +251,76 @@
             </div>
         </div>
     </footer>
+    <script>
+        // type
+        const checkboxestype = document.querySelectorAll(".type_room");
+        const hiddenInput = document.getElementById("selectedRoomTypes");
+
+        checkboxestype.forEach(checkbox => {
+            checkbox.addEventListener("change", function () {
+                let selectedValues = Array.from(checkboxestype)
+                    .filter(cb => cb.checked)
+                    .map(cb => cb.value);
+
+                hiddenInput.value = selectedValues.join(",");
+            });
+        });
+        // capacity
+        const capacityCheckboxes = document.querySelectorAll(".capacity_room");
+        const hiddenCapacityInput = document.getElementById("selectedRoomCapacities");
+
+        capacityCheckboxes.forEach(checkbox => {
+            checkbox.addEventListener("change", function () {
+                let selectedCapacities = Array.from(capacityCheckboxes)
+                    .filter(cb => cb.checked)
+                    .map(cb => cb.value);
+
+                hiddenCapacityInput.value = selectedCapacities.join(",");
+                console.log(hiddenCapacityInput.value);
+            });
+        });
+        // sort
+        const sortRadios = document.querySelectorAll(".sort_room");
+        const hiddenSortInput = document.getElementById("selectedSort");
+
+        sortRadios.forEach(radio => {
+            radio.addEventListener("change", function () {
+                hiddenSortInput.value = this.value;
+            });
+        });
+        // Gía
+        const minRange = document.getElementById("minRange");
+        const maxRange = document.getElementById("maxRange");
+        const minValueSpan = document.getElementById("minValue");
+        const maxValueSpan = document.getElementById("maxValue");
+        const minInput = document.getElementById("minInput");
+        const maxInput = document.getElementById("maxInput");
+
+        // Cập nhật giá trị hiển thị ban đầu
+        function updateValues() {
+            minValueSpan.textContent = minRange.value;
+            maxValueSpan.textContent = maxRange.value;
+            minInput.value = minRange.value;
+            maxInput.value = maxRange.value;
+        }
+
+        // Xử lý khi kéo thanh giá trị min
+        minRange.addEventListener("input", function () {
+            if (parseInt(minRange.value) > parseInt(maxRange.value)) {
+                minRange.value = maxRange.value;
+            }
+            updateValues();
+        });
+
+        // Xử lý khi kéo thanh giá trị max
+        maxRange.addEventListener("input", function () {
+            if (parseInt(maxRange.value) < parseInt(minRange.value)) {
+                maxRange.value = minRange.value;
+            }
+            updateValues();
+        });
+
+        updateValues();
+    </script>
 </body>
 </html>
